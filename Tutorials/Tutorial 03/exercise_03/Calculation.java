4ebc66e8c156e605c74a6b579d2f08d51388b0c4
kavindu-dilshan <kavindudilshan84@gmail.com>
2023-09-06 21:19:48 +0530

    final codes

---
+++ Tutorials/Tutorial 03/exercise_03/Calculation.java
@@ -0,0 +1,12 @@
+package exercise_03;
+
+public class Calculation {
+	
+	public static int add(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public static int substract(int num1, int num2) {
+		return num1 - num2;
+	}
+}
---
+++ Tutorials/Tutorial 03/exercise_03/DemoApp.java
@@ -0,0 +1,12 @@
+package exercise_03;
+
+public class DemoApp {
+	
+	public static void main(String[] args) {
+		int result1 = Calculation.add(5, 2);
+		int result2 = Calculation.substract(5, 2);
+		
+		System.out.println(result1);
+		System.out.println(result2);
+	}
+}
